58d0f317bd6dffa991291095cbcc9d8990ab8756
SanketImmunoACT <Sanket.rathod@immunoact.com>
2026-01-13 12:03:14 +0530

    chore: Remove Vercel Speed Insights integration
    - Remove @vercel/speed-insights dependency from package.json
    - Delete SpeedInsights import from App.jsx
    - Remove SpeedInsights component from Router
    - Fix indentation and formatting in App.jsx routes section
    - Simplify dependencies by removing unused performance monitoring package

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import { SpeedInsights } from '@vercel/speed-insights/react'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import ScrollToTop from '@/components/ScrollToTop'
@@ -55,7 +54,6 @@ function App() {
     <CookieProvider>
       <AuthProvider>
         <Router>
-          <SpeedInsights />
           <Toaster
             toastOptions={{
               // Default options for all toasts
@@ -93,107 +91,106 @@ function App() {
             }}
           />
           <Routes>
-          {/* Admin Routes */}
-          <Route path="/admin/login" element={<AdminLogin />} />
-          <Route path="/admin" element={
-            <ProtectedRoute>
-              <AdminLayout />
-            </ProtectedRoute>
-          }>
-            <Route index element={<Navigate to="/admin/dashboard" replace />} />
-            <Route path="dashboard" element={<AdminDashboard />} />
-            <Route path="media" element={
-              <ProtectedRoute requiredRoles={['super_admin', 'office_executive']}>
-                <MediaManagement />
+            {/* Admin Routes */}
+            <Route path="/admin/login" element={<AdminLogin />} />
+            <Route path="/admin" element={
+              <ProtectedRoute>
+                <AdminLayout />
               </ProtectedRoute>
-            } />
-            <Route path="publications" element={
-              <ProtectedRoute requiredRoles={['super_admin', 'office_executive']}>
-                <PublicationsManagement />
-              </ProtectedRoute>
-            } />
-            <Route path="careers" element={
-              <ProtectedRoute requiredRoles={['super_admin', 'hr_manager']}>
-                <CareersManagement />
-              </ProtectedRoute>
-            } />
-            <Route path="job-applications" element={
-              <ProtectedRoute requiredRoles={['super_admin', 'hr_manager']}>
-                <JobApplicationsManagement />
-              </ProtectedRoute>
-            } />
-            <Route path="contacts" element={
-              <ProtectedRoute requiredRoles={['super_admin', 'office_executive']}>
-                <ContactManagement />
-              </ProtectedRoute>
-            } />
-            <Route path="hospitals" element={
-              <ProtectedRoute requiredRoles={['super_admin']}>
-                <AdminHospitals />
-              </ProtectedRoute>
-            } />
-            <Route path="referrals" element={
-              <ProtectedRoute requiredRoles={['super_admin', 'hr_manager']}>
-                <ReferralManagement />
-              </ProtectedRoute>
-            } />
-            <Route path="*" element={<Navigate to="/admin/dashboard" replace />} />
-          </Route>
+            }>
+              <Route index element={<Navigate to="/admin/dashboard" replace />} />
+              <Route path="dashboard" element={<AdminDashboard />} />
+              <Route path="media" element={
+                <ProtectedRoute requiredRoles={['super_admin', 'office_executive']}>
+                  <MediaManagement />
+                </ProtectedRoute>
+              } />
+              <Route path="publications" element={
+                <ProtectedRoute requiredRoles={['super_admin', 'office_executive']}>
+                  <PublicationsManagement />
+                </ProtectedRoute>
+              } />
+              <Route path="careers" element={
+                <ProtectedRoute requiredRoles={['super_admin', 'hr_manager']}>
+                  <CareersManagement />
+                </ProtectedRoute>
+              } />
+              <Route path="job-applications" element={
+                <ProtectedRoute requiredRoles={['super_admin', 'hr_manager']}>
+                  <JobApplicationsManagement />
+                </ProtectedRoute>
+              } />
+              <Route path="contacts" element={
+                <ProtectedRoute requiredRoles={['super_admin', 'office_executive']}>
+                  <ContactManagement />
+                </ProtectedRoute>
+              } />
+              <Route path="hospitals" element={
+                <ProtectedRoute requiredRoles={['super_admin']}>
+                  <AdminHospitals />
+                </ProtectedRoute>
+              } />
+              <Route path="referrals" element={
+                <ProtectedRoute requiredRoles={['super_admin', 'hr_manager']}>
+                  <ReferralManagement />
+                </ProtectedRoute>
+              } />
+              <Route path="*" element={<Navigate to="/admin/dashboard" replace />} />
+            </Route>
 
-          {/* Public Routes */}
-          <Route path="/*" element={
-            <div className="min-h-screen flex flex-col bg-white font-futura">
-              <Header />
-              <main className="flex-1">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  
-                  {/* Main Page Routes */}
-                  <Route path="/about" element={<About />} />
-                  <Route path="/about/who-we-are" element={<WhoWeAre />} />
-                  <Route path="/about/team" element={<Team />} />
-                  
-                  <Route path="/science" element={<Science />} />
-                  <Route path="/science/research" element={<Research />} />
-                  <Route path="/science/process" element={<Science />} />
-                  <Route path="/science/pipeline" element={<Science />} />
-                  <Route path="/science/publications" element={<Science />} />
-                  
-                  <Route path="/nexcar19" element={<NexCAR19 />} />
-                  <Route path="/nexcar19/patients" element={<ForPatients />} />
-                  <Route path="/nexcar19/healthcare-professionals" element={<ForHCP />} />
-                  <Route path="/nexcar19/treatment-process" element={<NexCAR19 />} />
-                  <Route path="/nexcar19/partnered-treatment-centres" element={<PartneredHospitals />} />
-                  <Route path="/nexcar19-hcp" element={<NexCAR19 />} />
-                  <Route path="/hcp" element={<ForHCP />} />
-                  
-                  {/* Media Routes */}
-                  <Route path="/media" element={<NewsMedia />} />
-                  
-                  <Route path="/contact" element={<Contact />} />
-                  <Route path="/media/newsroom" element={<NewsMedia />} />
-                  <Route path="/philanthropy" element={<Philanthropy />} />
-                  <Route path="/philanthropy/immunoact-foundation" element={<Philanthropy />} />
-                  <Route path="/careers" element={<Careers />} />
-                  <Route path="/careers/current-job-openings" element={<Careers />} />
-                  <Route path="/publications" element={<Publications />} />
-                  <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-                  <Route path="/cookie-settings" element={<CookieSettings />} />
-                  <Route path="/partnered-hospitals" element={<PartneredHospitals />} />
-                  <Route path="/treatment-centres" element={<PartneredHospitals />} />
-                  <Route path="/collaborations" element={<Contact />} />
-                  <Route path="/employee-referral" element={<EmployeeReferral />} />
-                  <Route path="/sitemap" element={<Sitemap />} />
-                </Routes>
-              </main>
-              <Footer />
-              <ScrollToTop />
-              <CookieConsent />
-              <AnalyticsTracker />
-              <SpeedInsights />
-            </div>
-          } />
-        </Routes>
+            {/* Public Routes */}
+            <Route path="/*" element={
+              <div className="min-h-screen flex flex-col bg-white font-futura">
+                <Header />
+                <main className="flex-1">
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+
+                    {/* Main Page Routes */}
+                    <Route path="/about" element={<About />} />
+                    <Route path="/about/who-we-are" element={<WhoWeAre />} />
+                    <Route path="/about/team" element={<Team />} />
+
+                    <Route path="/science" element={<Science />} />
+                    <Route path="/science/research" element={<Research />} />
+                    <Route path="/science/process" element={<Science />} />
+                    <Route path="/science/pipeline" element={<Science />} />
+                    <Route path="/science/publications" element={<Science />} />
+
+                    <Route path="/nexcar19" element={<NexCAR19 />} />
+                    <Route path="/nexcar19/patients" element={<ForPatients />} />
+                    <Route path="/nexcar19/healthcare-professionals" element={<ForHCP />} />
+                    <Route path="/nexcar19/treatment-process" element={<NexCAR19 />} />
+                    <Route path="/nexcar19/partnered-treatment-centres" element={<PartneredHospitals />} />
+                    <Route path="/nexcar19-hcp" element={<NexCAR19 />} />
+                    <Route path="/hcp" element={<ForHCP />} />
+
+                    {/* Media Routes */}
+                    <Route path="/media" element={<NewsMedia />} />
+
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/media/newsroom" element={<NewsMedia />} />
+                    <Route path="/philanthropy" element={<Philanthropy />} />
+                    <Route path="/philanthropy/immunoact-foundation" element={<Philanthropy />} />
+                    <Route path="/careers" element={<Careers />} />
+                    <Route path="/careers/current-job-openings" element={<Careers />} />
+                    <Route path="/publications" element={<Publications />} />
+                    <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                    <Route path="/cookie-settings" element={<CookieSettings />} />
+                    <Route path="/partnered-hospitals" element={<PartneredHospitals />} />
+                    <Route path="/treatment-centres" element={<PartneredHospitals />} />
+                    <Route path="/collaborations" element={<Contact />} />
+                    <Route path="/employee-referral" element={<EmployeeReferral />} />
+                    <Route path="/sitemap" element={<Sitemap />} />
+                  </Routes>
+                </main>
+                <Footer />
+                <ScrollToTop />
+                <CookieConsent />
+                <AnalyticsTracker />
+              </div>
+            } />
+          </Routes>
         </Router>
       </AuthProvider>
     </CookieProvider>
